b58a170120460761ef29181ebac2db3657bcd8fc
AntonioAbilio <55913637+AntonioAbilio@users.noreply.github.com>
2024-11-06 10:26:52 +0000

    Changed upload_image

--- upload_image.sh
+++ upload_image.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Replace with your group's image name
-IMAGE_NAME=gitlab.up.pt:5050/lbaw/lbawYYYY/lbawYYXX
+IMAGE_NAME=gitlab.up.pt:5050/lbaw/lbaw2425/lbaw24113
 
 # Ensure that dependencies are available
 composer install
@@ -12,6 +12,6 @@ php artisan config:clear
 php artisan clear-compiled
 php artisan optimize
 
-# docker buildx build --push --platform linux/amd64 -t $IMAGE_NAME .
+#docker buildx build --push --platform linux/amd64 -t $IMAGE_NAME .
 docker build -t $IMAGE_NAME .
 docker push $IMAGE_NAME
