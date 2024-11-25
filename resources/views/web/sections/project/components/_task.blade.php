aeff8b314b600bb643585c2794bb0ddf3c9a10d1
João Santos <jpnsantos14@gmail.com>
2024-11-25 14:34:55 +0000

    adding highlight

--- resources/views/web/sections/project/components/_task.blade.php
+++ resources/views/web/sections/project/components/_task.blade.php
@@ -1,11 +1,12 @@
-<div class="p-4 bg-gray-100 rounded-md shadow-sm">
+<div class="p-4 bg-gray-100 rounded-md shadow-sm task-card @if ($task->assigned_to === Auth::id()) bg-yellow-100 @endif"
+    data-assigned-to="{{ $task->assigned_to }}">
     <h4 class="text-lg font-semibold text-gray-800">{{ $task->title }}</h4>
     <div class="text-sm text-gray-600 flex items-center gap-2 mt-2 justify-between flex-wrap">
-    @if($task->assignedDeveloper && $task->assignedDeveloper->user)
-        <x-user :user="$task->assignedDeveloper->user" />
-    @else
-        <p>No user assigned.</p>
-    @endif    
+        @if ($task->assignedDeveloper && $task->assignedDeveloper->user)
+            <x-user :user="$task->assignedDeveloper->user" />
+        @else
+            <p>No user assigned.</p>
+        @endif
         <div class="space-y-2">
             <span
                 class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800">
