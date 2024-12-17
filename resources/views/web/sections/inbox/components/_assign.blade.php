c730b85a516f09a0f2c9a8aaf503158003008e15
up202207919 <up202207919@edu.fe.up.pt>
2024-12-17 16:49:39 +0000

    Refactored notifications and fixed visual bug

--- resources/views/web/sections/inbox/components/_assign.blade.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $task = \App\Models\Task::find($notification->task_id)->title; 
-    $project = \App\Models\Project::find($notification->project_id)->title;      
-?>
-You got assigned to task "<?=$task?>" in project <?=$project?>.
